062eebac7809ce1dd7c60b8f900bec9fbf37f0c6
Danillo Vieira <danillodesalesvieira@gmail.com>
2024-02-21 16:03:52 -0300

    feat: limpar campo ao errar e limpar mensagem ao solicitar novo número

--- LogicadeProgramacao2/numero_misterioso2.js
+++ LogicadeProgramacao2/numero_misterioso2.js
@@ -9,28 +9,38 @@ const exibirTextoNaTela = (tag,texto) =>{
 }
 
 const verificacao = () => {
-    let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
     let chute = document.querySelector('input').value;
     if (chute === ''){
         exibirTextoNaTela('h2','');
     }else{
         chute = parseInt(chute);
         tentativa+=1;
+        let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
         if(chute === numeroSecreto){
         exibirTextoNaTela('h2',`Acertou! O número secreto é ${numeroSecreto}. Você utilizou um total de ${tentativa} ${palavraTentativa}.`);
         }else if (chute > numeroSecreto){
         exibirTextoNaTela('h2',`Errou! O número é menor que ${chute}!`);
+        limparCampo();
         }else{
         console.log(numeroSecreto);
         exibirTextoNaTela('h2',`Errou! O número é maior que ${chute}!`);
+        limparCampo();
         }
+        limparCampo();
     }
 }
 
 const novoNumero = () => {
     numeroSecreto = parseInt(Math.random()*100 + 1);
     console.log(numeroSecreto);
+    exibirTextoNaTela('h2','');
 }
+
+const limparCampo = () => {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
 
 
 
